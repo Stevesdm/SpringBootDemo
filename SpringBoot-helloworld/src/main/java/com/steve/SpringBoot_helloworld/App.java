bafdb8707a0df0cdff18a2e378063fa1c732d9dd
Steve jobson <stevejobson@163.com>
2017-03-04 11:09:00 +0800

    add  springboot hello world

---
+++ SpringBoot-helloworld/src/main/java/com/steve/SpringBoot_helloworld/App.java
@@ -0,0 +1,26 @@
+package com.steve.SpringBoot_helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+
+@RestController
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+    
+    @RequestMapping("/")
+    public String hello(){
+    	return "hello world";
+    }
+}

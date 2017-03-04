a462bd655b1cbf2931997ca03b6e3df4490ae8ad
Steve jobson <stevejobson@163.com>
2017-03-04 11:55:33 +0800

    Add return json data

--- SpringBoot-helloworld/src/main/java/com/steve/SpringBoot_helloworld/App.java
+++ SpringBoot-helloworld/src/main/java/com/steve/SpringBoot_helloworld/App.java
@@ -2,16 +2,19 @@ package com.steve.SpringBoot_helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.steve.model.Person;
 
 /**
  * Hello world!
  *
  */
 
-@RestController
 @SpringBootApplication
+@ComponentScan("com.steve")
 public class App 
 {
     public static void main( String[] args )
@@ -19,8 +22,5 @@ public class App
     	SpringApplication.run(App.class, args);
     }
     
-    @RequestMapping("/")
-    public String hello(){
-    	return "hello world";
-    }
+ 
 }
---
+++ SpringBoot-helloworld/src/main/java/com/steve/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.steve.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.steve.model.Person;
+
+@RestController
+public class TestController {
+
+	   @RequestMapping("/")
+	    public String hello(){
+	    	return "hello world";
+	    }
+	    
+	    @RequestMapping("/hello")
+		public Person getPerson(){
+			Person p =new Person();
+			p.setName("steve");
+			p.setAge(18);
+			return p;
+		}
+	
+}

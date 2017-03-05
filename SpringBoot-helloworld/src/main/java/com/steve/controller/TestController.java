7ed32c5f7b7f9bc9b5a2cbda0e8b01cc53fbe8d4
Steve jobson <stevejobson@163.com>
2017-03-05 15:57:30 +0800

    Change devtools configuration

--- SpringBoot-helloworld/src/main/java/com/steve/controller/TestController.java
+++ SpringBoot-helloworld/src/main/java/com/steve/controller/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
 
 	   @RequestMapping("/")
 	    public String hello(){
-	    	return "hello world";
+	    	return "hello world!!";
 	    }
 	    
 	    @RequestMapping("/hello")

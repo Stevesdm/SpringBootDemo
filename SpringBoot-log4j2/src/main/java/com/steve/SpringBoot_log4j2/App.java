94537716265cc1c4ee157faecf0e18c619901c56
Steve jobson <stevejobson@163.com>
2017-03-07 14:04:09 +0800

    Add devtools

---
+++ SpringBoot-log4j2/src/main/java/com/steve/SpringBoot_log4j2/App.java
@@ -0,0 +1,21 @@
+package com.steve.SpringBoot_log4j2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+	
+	
+    public static void main( String[] args )
+    {
+    	
+    	SpringApplication.run(App.class, args);
+    }
+    
+}

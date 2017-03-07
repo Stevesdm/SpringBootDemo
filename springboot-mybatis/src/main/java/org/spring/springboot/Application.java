9a32df436eaf03d5ea15e57a3cdae132cc071cd7
Steve jobson <stevejobson@163.com>
2017-03-07 18:13:31 +0800

    Change some configuration

---
+++ springboot-mybatis/src/main/java/org/spring/springboot/Application.java
@@ -0,0 +1,14 @@
+package org.spring.springboot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("org.spring.springboot.dao")
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
